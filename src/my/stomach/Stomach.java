ce7ecc088ff536cfd56a14cb213f50b078d3d8bb
yyin-talend <yyin@talend.com>
2014-06-10 13:51:17 +0800

    add DanDanMian in the list

--- src/my/stomach/Stomach.java
+++ src/my/stomach/Stomach.java
@@ -17,6 +17,7 @@ public class Stomach {
 	public Stomach() {
 		foodsInside.add(new Pizza());
 		/* Exercise 2 : add your food */
+		foodsInside.add(new DanDanMian());
 	}
 	@Override
 	public String toString() {
