45e1248be98ce3bffe42b5caf7be731198b6a62b
msjian <msjian@talend.com>
2014-06-10 16:03:30 +0800

    change Pizza class

--- src/my/food/Pizza.java
+++ src/my/food/Pizza.java
@@ -17,7 +17,7 @@ public class Pizza implements Food {
 	 ingredients.add("mozzarella");
 	 ingredients.add("tomato");
 	 ingredients.add("mushroom"); //so good
-	 ingredients.add("mushroom"); //one more
+	 ingredients.add("mushroo"); //one more
 	}
 	
 	@Override
